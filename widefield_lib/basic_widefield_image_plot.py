99780419bd323282710dabc993498944b753d9e9
quantech <quantech@iisbg.local>
2023-01-12 16:58:34 +0100

    Codebase cleanup after showing (possibly) working ODMR on 12.01.2023 with Christians sample.

--- widefield_lib/basic_widefield_image_plot.py
+++ widefield_lib/basic_widefield_image_plot.py
@@ -4,7 +4,7 @@ import datetime
 import matplotlib
 import os
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-from Archiv.OLD_CODE_BASE_BEFORE_DATA_STRUCTURE.lib.utilities import save_data_to_json as save
+from configuration_widefield import f_vec_array
 #matplotlib.use("TkAgg")
 
 font = {
@@ -16,7 +16,7 @@ matplotlib.rc('font', **font)
 roi_size = 1024  # pixels
 
 work_dir = 'D:\\Widefield\\Daten\\telescope_max_focus\\'
-file_name = '9_point_odmr_12012023_long'
+file_name = '9_point_odmr_12012023_long_repeat'
 path = work_dir + file_name + '.raw'
 n_f = 9
 n_avg = 1500
@@ -29,18 +29,26 @@ def sweep(path):
     for f in range(0,n_f):
         print(f'Reading in step {f} with MW on')
         for n in range(0, n_avg):
-            print(f"{(n+1)*(f+1)}", end='\r', flush=True)
+            print(f"{f*2*n_avg + n}", end='\r', flush=True)
             shift = 2*f*n_avg*chunk_size + n*chunk_size
-            imgMWON_sum[f, :, :] += np.fromfile(path, dtype='uint16', sep="", count=chunk_size,
-                                             offset=shift*2).reshape(1024,
-                                                                   1024).astype(int)  # mal 2, da jeder Eintrag 16 statt 8 bit is
+            try:
+                imgMWON_sum[f, :, :] += np.fromfile(path, dtype='uint16', sep="", count=chunk_size,
+                                                 offset=shift*2).reshape(1024,
+                                                                       1024).astype(int)  # mal 2, da jeder Eintrag 16 statt 8 bit is
+            except ValueError:
+                print(f"MISSING DATA in Frame {f*2*n_avg + n}")
+                pass
         print(f'Reading in step {f} with MW off')
         for n in range(n_avg, 2*n_avg):
-            print(f"Frame Nr. {(n+1)*(f+1)}", end='\r', flush=True)
+            print(f"Frame Nr. {f*2*n_avg + n}", end='\r', flush=True)
             shift = 2*f * n_avg * chunk_size + n * chunk_size
-            imgMWOFF_sum[f, :, :] += np.fromfile(path, dtype='uint16', sep="", count=chunk_size,
-                                                offset=shift * 2).reshape(1024,
-                                                                          1024).astype(int)  # mal 2, da jeder Eintrag 16 statt 8 bit is
+            try:
+                imgMWOFF_sum[f, :, :] += np.fromfile(path, dtype='uint16', sep="", count=chunk_size,
+                                                    offset=shift * 2).reshape(1024,
+                                                                              1024).astype(int)  # mal 2, da jeder Eintrag 16 statt 8 bit is
+            except ValueError:
+                print(f"MISSING DATA in Frame {f*2*n_avg + n}")
+                pass
     np.savez(f'{file_name}.npz', mw_on=imgMWON_sum, mw_off=imgMWOFF_sum)
     return imgMWON_sum, imgMWOFF_sum
 
@@ -58,13 +66,17 @@ divider = make_axes_locatable(ax2)
 cax = divider.append_axes('right', size='5%', pad=0.05)
 bin_width = 1
 mw= mw_on/mw_off#1 - mw_on/mw_off
-y = [x.reshape(roi_size//bin_width, bin_width, roi_size//bin_width, bin_width).sum(3).sum(1) for x in mw]
+y = np.array([x.reshape(roi_size//bin_width, bin_width, roi_size//bin_width, bin_width).sum(3).sum(1) for x in mw]) / bin_width**2
 
-x_idx = [int(x) for x in np.array([50])/64 * y[0].shape[0]] + [420, np.random.randint(1024), np.random.randint(1024), np.random.randint(1024), np.random.randint(1024)]
-y_idx = [int(x) for x in np.array([10])/64 * y[0].shape[1]] + [420, np.random.randint(1024), np.random.randint(1024), np.random.randint(1024), np.random.randint(1024)]
+x_idx = [int(x) for x in np.array(
+    [50 / 60 * 1024, 430, np.random.randint(1024), np.random.randint(1024), np.random.randint(1024),
+     np.random.randint(1024)]) / 1024 * y[0].shape[0]]
+y_idx = [int(x) for x in np.array(
+    [10 / 60 * 1024, 418, np.random.randint(1024), np.random.randint(1024), np.random.randint(1024),
+     np.random.randint(1024)]) / 1024 * y[0].shape[1]]
 for x_i, y_i in zip(x_idx, y_idx):
     odmr = np.array([k[x_i, y_i] for k in y])
-    ax.plot(odmr, zorder=1)
+    ax.plot(f_vec_array, odmr, zorder=1)
     ax2.plot(x_i, y_i, 'o', zorder=2)
     ax.set_xlabel('Frequency [GHz]')
 
@@ -72,7 +84,7 @@ while 1:
     for idx in range(0, n_f):
         im = ax2.imshow((1-y[idx])*100, zorder=1, vmin=0, vmax=5)
         fig.colorbar(im, cax=cax, orientation='vertical')
-        li = ax.axvline(idx, label=f'{idx}', zorder=2)
+        li = ax.axvline(f_vec_array[idx], label=f'{idx}', zorder=2)
         plt.pause(0.5)
         im.remove()
         li.remove()
